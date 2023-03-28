80a289c1ae64b29ebf550628bbf5248545a89441
LipninNikita <lipnin.ne@it-rkomi.ru>
2023-03-28 11:57:25 +0300

    65

---
+++ 65. Valid Number(h)/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(IsNumber("Infinity"));
+    }
+    public static bool IsNumber(string s)
+    {
+        bool status = true;
+
+        if (s.Contains("Infinity"))
+            return false;
+
+        try
+        {
+            var i = Convert.ToDouble(s);
+        }
+        catch (System.FormatException)
+        {
+            status = false;
+        }
+        catch (System.OverflowException)
+        {
+            status = true;
+        }
+        return status;
+    }
+}
